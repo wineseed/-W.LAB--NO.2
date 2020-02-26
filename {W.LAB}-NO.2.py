894a5e50f3259f26d0140678d9ea959a49354b8a
wineseed <51790938+wineseed@users.noreply.github.com>
2020-02-26 18:10:08 +0900

    Update {W.LAB}-NO.2.py

--- {W.LAB}-NO.2.py
+++ {W.LAB}-NO.2.py
@@ -1,27 +1,57 @@
 import discord
-import random
-import os
-d = discord.Client()
+import asyncio
+from discord.ext import commands
+
+app = commands.Bot(command_prefix = "!")
+
+@app.event
 async def on_ready():
-    print("login.....")
+    print("접속 완료")
 
+@app.command()
+async def 클(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f"{amount} 만큼 삭제됨")
+    await asyncio.sleep(3)
+    await ctx.channel.purge(limit=1)
 
+@app.command()
+async def 서버정보(ctx):
+    await ctx.send('''
+가입일:2020/02/25
+서버네임:닥이들
+서버 위치:남한
+잠수 채널:없음
+서버 책임자:닥돌
+봇 연구자:준영맨,인템,타나르,닥돌
+봇의 수:5
+연결된 서버:슈퍼준영맨
+등업방법:!붙이고 등업 [자신의 정보]
+''')
 
-@d.event
+@app.command()
+async def 역할(ctx):
+    await ctx.send('''역할 종류:
+DAKDOL
+봇 연구자
+스크림 팀
+봇들
+트위치 팔로워
+구독자
+등업 희망자
+채팅정지자''')
 
-async def on_message(message):
-    if message.content.startswith("!등업"):
+@app.command()
+async def 보안(ctx):
+    await ctx.send('''보안강도:4단계
+                 이메일 인증,가입 5분후,서버 가입 10분 후
+                 콘텐츠 스캔 X''')
+@app.command()
+async def 등업(ctx):
+    await ctx.send('감사합니다.관리자에게 전달되었으니 조금만 기다려주세요')
 
-         msg = message.content[3:]
-    if message.is_private and message.author != '602093599877890048':
-        await d.send_message(discord.utills.get(d.get_all_members(), id='540416346962264073'), message.author.name+msg)
+@app.command()
+async def DM(ctx):
+    await ctx.send('죄송합니다. 아직 연구중이니 조금만 기다려 주세요')
 
-         
-
-         
-
-    
-
-access_token = os.inviron["BOT_TOKEN"]
-d.run(access_token)
-
+app.run('BOT_TOKEN')
